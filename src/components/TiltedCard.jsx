9a7930467f126e88bd01bfa8f660d9b29de32471
Quoc Khanh <qckhanh205@gmail.com>
2026-09-03 23:15:17 +0700

    feat: replace github repo link with shields.io badge

--- src/components/TiltedCard.jsx
+++ src/components/TiltedCard.jsx
@@ -110,7 +110,7 @@ export default function TiltedCard({
         />
 
         {displayOverlayContent && overlayContent && (
-          <motion.div className="absolute top-0 left-0 z-[2] will-change-transform [transform:translateZ(30px)]">
+          <motion.div className="absolute top-0 left-0 w-full h-full z-[2] will-change-transform [transform:translateZ(30px)]">
             {overlayContent}
           </motion.div>
         )}
